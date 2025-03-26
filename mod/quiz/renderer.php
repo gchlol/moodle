5362dec991b72128fcbf8cb87d0476c75731fe02
Nicholas Lambell <nicholas.lambell@health.qld.gov.au>
2025-03-26 10:51:21 +1000

    Only show extra confirmation popup if there are unanswered quiz questions

--- mod/quiz/renderer.php
+++ mod/quiz/renderer.php
@@ -793,8 +793,12 @@ class mod_quiz_renderer extends plugin_renderer_base {
             if ($attemptobj->get_quiz()->navmethod == 'free') {
                 // Only count the unanswered question if the navigation method is set to free.
                 $totalunanswered = $attemptobj->get_number_of_unanswered_questions();
+
+                // GCHLOL: Only show extra confirmation popup if there are unanswered questions.
+                if ($totalunanswered > 0) {
+                    $this->page->requires->js_call_amd('mod_quiz/submission_confirmation', 'init', [ $totalunanswered ]);
+                }
             }
-            $this->page->requires->js_call_amd('mod_quiz/submission_confirmation', 'init', [$totalunanswered]);
         }
         $button->primary = true;
 
